98b36dc206995cc4b5f9d9e98daad9a149c7453b
yuichiro fukagawa <yuichiro@fk1.jp>
2017-10-17 03:04:34 +0900

    add move

--- gaw01/src/app.js
+++ gaw01/src/app.js
@@ -7,28 +7,14 @@ var HelloWorldLayer = cc.Layer.extend({
     blue: cc.color(0, 0, 255),
     clear:cc.color(0, 0, 0, 0),
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
         var size = cc.winSize;
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
         var helloLabel = new cc.LabelTTF("tap icons!", "Arial", 38);
         // position the label on the center of the screen
         helloLabel.x = size.width / 2;
         helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
         this.addChild(helloLabel, 5);
 
-        // add "HelloWorld" splash screen"
         this.sprite = new cc.Sprite(res.HelloWorld_png);
         this.sprite.attr({
             x: size.width / 2,
@@ -36,7 +22,7 @@ var HelloWorldLayer = cc.Layer.extend({
         });
         this.addChild(this.sprite, 0);
 
-        //var boxes = new Array(5);
+        //var boxes = new Array();
         for (var i = 0 ; i < 6 ; i++ ){
         this.boxes[i] = new Array();
         for (var j = 0 ; j < 9 ; j++ ){
@@ -59,15 +45,18 @@ var HelloWorldLayer = cc.Layer.extend({
 
 var listener = cc.EventListener.create({
     event: cc.EventListener.TOUCH_ONE_BY_ONE,
- 
+    start_position: {x:0 , y:0},
+    end_position: {x:0 , y:0},
     onTouchBegan: function (touch, event) {
         var target = event.getCurrentTarget();
+        this.start_position = touch.getLocation();
         var location = target.convertToNodeSpace(touch.getLocation());
         var spriteSize = target.getContentSize();
         var spriteRect = cc.rect(0, 0, spriteSize.width, spriteSize.height);
         if (cc.rectContainsPoint(spriteRect, location)) {
+            cc.log(this.start_position);
             var tag = target.getTag();
-            target.removeFromParent();
+            
             cc.log("onTouchBegan ", tag);
             return true;
         }
@@ -75,11 +64,29 @@ var listener = cc.EventListener.create({
     },
  
     onTouchMoved: function (touch, event) {
+        this.end_position = touch.getLocation();
         cc.log("onTouchMoved");
     },
  
     onTouchEnded: function (touch, event) {
-        cc.log("onTouchEnded");
+        cc.log("onTouchEnded",this.end_position);
+        var target = event.getCurrentTarget();
+        var tag = target.getTag();
+        var diff = { 
+            x: this.end_position.x - this.start_position.x,
+            y: this.end_position.y - this.start_position.y
+        };
+        var vector = {
+            x: 2000 * diff.x / Math.sqrt( Math.pow(diff.x,2) + Math.pow(diff.y,2)),
+            y: 2000 * diff.y / Math.sqrt( Math.pow(diff.x,2) + Math.pow(diff.y,2))
+        };
+        vector.x += this.start_position.x;
+        vector.y += this.start_position.y;
+        var moves = cc.MoveTo.create(1,vector);
+        target.runAction(cc.Sequence.create(moves));
+        setInterval(function() {
+            target.removeFromParent();
+        }, 1000);
     },
  
     onTouchCancelled: function (touch, event) {
